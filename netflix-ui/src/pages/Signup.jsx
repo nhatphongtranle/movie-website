9f13b0212de254b471b20e80097817e2f6dd15b4
nhatphongtranle <kudo40664869@gmail.com>
2023-07-26 16:36:05 +0700

    Signup Design

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,5 +1,41 @@
 import React from "react";
-
+import styled from "styled-components";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <BackgroundImage />
+      <Header />
+      <div className="body flex column a-center j-center">
+        <div className="text flex column">
+          <h1>Unlimited movies, TV shows and more</h1>
+          <h4>Watch anywhere. Cancel anytime.</h4>
+          <h6>
+            Ready to watch? Enter your email to create or restart membership
+          </h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder="Email Address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Get Started</button>
+        </div>
+        <button>Log in</button>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rbga(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+  }
+`;
