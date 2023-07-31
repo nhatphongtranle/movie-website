a0a9c94dec4713ab33f659fc7d7b9bc7b3f02703
nhatphongtranle <kudo40664869@gmail.com>
2023-07-31 22:15:23 +0700

    Navbar

---
+++ netflix-ui/src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { FaSearch } from "react-icons/fa";
+export default function Navbar({ isScrolled }) {
+  const links = [
+    { name: "Home", link: "/" },
+    { name: "TV Shows", link: "/tv" },
+    { name: "Movies", link: "/movies" },
+    { name: "My List", link: "/mylist" },
+  ];
+  const [showSearch, setShowSearch] = useState(false);
+  const [inputHover, setInputHover] = useState(false);
+  return (
+    <Container>
+      <nav className={`flex ${isScrolled ? "Scrolled" : ""}`}>
+        <div className="left flex a-center">
+          <div className="brand flex a-center j-center">
+            <img src={logo} alt="logo" />
+          </div>
+          <ul className="link flex">
+            {links.map((name, link) => {
+              return (
+                <li key={name}>
+                  <Link to={link}>{name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="right flex a-center">
+          <div className={`search ${showSearch ? "show-search" : ""}`}>
+            <button
+              onFocus={() => setShowSearch(true)}
+              onBlur={() => {
+                if (!inputHover) setShowSearch(false);
+              }}
+            >
+              <FaSearch />
+            </button>
+          </div>
+        </div>
+      </nav>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
