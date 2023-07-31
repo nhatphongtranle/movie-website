7c4b53c8a15cc965bce7fb3616c755686da3b664
nhatphongtranle <kudo40664869@gmail.com>
2023-07-31 22:16:33 +0700

    First commit

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -5,9 +5,11 @@ export default function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null)
-  }
-  return <div>
-    <Navbar isScrolled={isScrolled} />
-  </div>;
+    return () => (window.onscroll = null);
+  };
+  return (
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
+  );
 }
