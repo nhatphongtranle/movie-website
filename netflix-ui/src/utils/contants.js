284600ed3aa009b7b5ceb7867e5230125820f525
nhatphongtranle <kudo40664869@gmail.com>
2023-08-07 21:43:15 +0700

    API

--- netflix-ui/src/utils/contants.js
+++ netflix-ui/src/utils/contants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "ab5d9a50a13ea820d8f1396403bb1f9b";
-export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
