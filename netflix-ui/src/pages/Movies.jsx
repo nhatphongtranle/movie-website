c4f0af36f0f8d7d30e791caf7f0de99232279aed
nhatphongtranle <kudo40664869@gmail.com>
2023-08-11 20:04:32 +0700

    Commit

---
+++ netflix-ui/src/pages/Movies.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import CardSlider from "../components/CardSlider";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
+import SelectGenre from "../components/SelectGenre";
+import Slider from "../components/Slider";
+import NotAvailable from "../components/NotAvailable";
+
+export default function Movies() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies);
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "movies" }));
+    }
+  }, [genresLoaded]);
+
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    //  if (currentUser) navigate("/");
+  });
+
+  return (
+    <Container>
+      <div className="navbar">
+        <Navbar isScrolled={isScrolled} />
+      </div>
+      <div className="data">
+        <SelectGenre genres={genres} type="movie" />
+        {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .data {
+    margin-top: 8rem;
+    .not-available {
+      text-align: center;
+      color: white;
+      margin-top: 4rem;
+    }
+  }
+`;
