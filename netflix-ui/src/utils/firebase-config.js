a18485e07c42cdcce564d02a736d3e089eea8af1
nhatphongtranle <kudo40664869@gmail.com>
2024-03-19 20:51:38 +0700

    add: firebase-config

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4QUylzATbv2yCQOxKtELjiTZtSqQzDe8",
-  authDomain: "react-movie-website-6eadf.firebaseapp.com",
-  projectId: "react-movie-website-6eadf",
-  storageBucket: "react-movie-website-6eadf.appspot.com",
-  messagingSenderId: "980737230653",
-  appId: "1:980737230653:web:ebc199840211bd9e206754",
-  measurementId: "G-J3YC0R8ZFG"
+  apiKey: "AIzaSyCPjXQNmjZWkDgMj1egFWDrjyS5kDCFPGY",
+  authDomain: "movie-website-c10aa.firebaseapp.com",
+  projectId: "movie-website-c10aa",
+  storageBucket: "movie-website-c10aa.appspot.com",
+  messagingSenderId: "620477274088",
+  appId: "1:620477274088:web:9e1fca83b8c4f1a0b3765d",
+  measurementId: "G-KPJBLJR8LN"
 };
 
 const app = initializeApp(firebaseConfig);
