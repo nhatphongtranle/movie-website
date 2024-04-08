42885d07fbe968c033b8a37fcd67a3fcbce0ba69
nhatphongtranle <kudo40664869@gmail.com>
2024-04-08 20:35:43 +0700

    Build: FooterLogin

--- netflix-ui/src/components/FooterLogin.jsx
+++ netflix-ui/src/components/FooterLogin.jsx
@@ -14,7 +14,7 @@ export default function FooterLogin() {
             </div>
             <div className="item-2">
               <div className="item-body">
-                <ul className="container">
+                <ul className="item-container">
                   <li className="list-item">
                     <a className="footer-link" href="https://help.netflix.com/support/412">
                       FAQ
@@ -47,8 +47,7 @@ export default function FooterLogin() {
                   </li>
                 </ul>
               </div>
-            </div>
-            <div className="wrapper"></div>
+            </div>  
           </div>
         </div>
       </footer>
@@ -57,6 +56,7 @@ export default function FooterLogin() {
 }
 
 const Footer = styled.div`
+  height: 17.5rem;
   background-color: rgba(0, 0, 0, 0.7);
   border-top: 1px solid rgba(128, 128, 128, 0.7);
   .Footer-body {
@@ -64,96 +64,72 @@ const Footer = styled.div`
     margin: auto;
     font-size: 1rem;
     font-weight: 400;
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
+    margin: 3rem 0 3rem 0;
+    padding: 0 2rem 0 2rem;
     .Footer-content {
       box-sizing: border-box;
       width: 100%;
       display: inherit;
       height: auto;
       .container {
-        display: -webkit-inline-box;
-        display: -webkit-inline-flex;
-        display: -ms-inline-flexbox;
         display: inline-flex;
-        -webkit-box-flex-wrap: wrap;
-        -webkit-flex-wrap: wrap;
-        -ms-flex-wrap: wrap;
         flex-wrap: wrap;
-        width: inherit;
         height: inherit;
         margin-top: -0px;
         margin-left: -0px;
         width: 100%;
-        -webkit-flex-direction: row;
-        -ms-flex-direction: row;
         flex-direction: row;
         padding: 0px;
-        .item-1,
-        .item-2 {
-          -webkit-flex: 0 0 100%;
-          -ms-flex: 0 0 100%;
-          flex: 0 0 100%;
-          display: -webkit-inline-box;
-          display: -webkit-inline-flex;
-          display: -ms-inline-flexbox;
+        .item-1 {
           display: inline-flex;
-          -webkit-box-flex-wrap: wrap;
-          -webkit-flex-wrap: wrap;
-          -ms-flex-wrap: wrap;
           flex-wrap: wrap;
           width: inherit;
           padding: 0px;
-        }
-        .item-content {
-          margin-bottom: 0.75rem;
-          a {
-            color: rgba(255, 255, 255, 0.7);
-            border-radius: 0.125rem;
+          .item-content {
+            margin-bottom: 0.75rem;
+            a {
+              color: rgba(255, 255, 255, 0.7);
+              border-radius: 0.125rem;
+            }
           }
         }
-        .item-2 .item-body {
-          margin: 0.75rem 0;
-          width: 100%;
-          font-size: 0.875rem;
-          font-weight: 400;
-          .container {
-            display: -webkit-inline-box;
-            display: -webkit-inline-flex;
-            display: -ms-inline-flexbox;
-            display: inline-flex;
-            -webkit-box-flex-wrap: wrap;
-            -webkit-flex-wrap: wrap;
-            -ms-flex-wrap: wrap;
-            flex-wrap: wrap;
-            width: inherit;
-            height: inherit;
-            margin-top: -1rem;
-            margin-left: -0.75rem;
-            width: calc(100% + 0.75rem);
-            -webkit-flex-direction: row;
-            -ms-flex-direction: row;
-            flex-direction: row;
-            padding: 0px;
-            .list-item {
-              display: -webkit-inline-box;
-              display: -webkit-inline-flex;
-              display: -ms-inline-flexbox;
+        .item-2 {
+          display: inline-flex;
+          flex-wrap: wrap;
+          width: inherit;
+          padding: 0px;
+          .item-body {
+            margin: 0.75rem 0;
+            width: 100%;
+            font-size: 0.875rem;
+            font-weight: 400;
+            .item-container {
               display: inline-flex;
-              -webkit-box-flex-wrap: wrap;
-              -webkit-flex-wrap: wrap;
-              -ms-flex-wrap: wrap;
               flex-wrap: wrap;
-              width: inherit;
+              height: inherit;
+              margin-top: -1rem;
+              margin-left: -0.75rem;
+              width: calc(100% + 0.75rem);
+              flex-direction: row;
               padding: 0px;
-              -webkit-flex: 0 0 calc(50% - 0.75rem);
-              -ms-flex: 0 0 calc(50% - 0.75rem);
-              flex: 0 0 calc(50% - 0.75rem);
-              margin-top: 1rem;
-              margin-left: 0.75rem;
-              margin-bottom: 5px;
+              .list-item {
+                display: -webkit-inline-box;
+                display: -webkit-inline-flex;
+                display: -ms-inline-flexbox;
+                display: inline-flex;
+                -webkit-box-flex-wrap: wrap;
+                -webkit-flex-wrap: wrap;
+                -ms-flex-wrap: wrap;
+                flex-wrap: wrap;
+                width: inherit;
+                padding: 0px;
+                -webkit-flex: 0 0 calc(50% - 0.75rem);
+                -ms-flex: 0 0 calc(50% - 0.75rem);
+                flex: 0 0 calc(50% - 0.75rem);
+                margin-top: 1rem;
+                margin-left: 0.75rem;
+                margin-bottom: 14px;
+              }
             }
           }
         }
