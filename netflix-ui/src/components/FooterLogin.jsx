f0370d85bbca363363bdca8c2e4527c81ad663fd
nhatphongtranle <kudo40664869@gmail.com>
2024-04-10 21:44:01 +0700

    ref: CSS for FooterLogin

--- netflix-ui/src/components/FooterLogin.jsx
+++ netflix-ui/src/components/FooterLogin.jsx
@@ -47,7 +47,7 @@ export default function FooterLogin() {
                   </li>
                 </ul>
               </div>
-            </div>  
+            </div>
           </div>
         </div>
       </footer>
@@ -55,85 +55,4 @@ export default function FooterLogin() {
   );
 }
 
-const Footer = styled.div`
-  height: 17.5rem;
-  background-color: rgba(0, 0, 0, 0.7);
-  border-top: 1px solid rgba(128, 128, 128, 0.7);
-  .Footer-body {
-    color: rgba(255, 255, 255, 0.7);
-    margin: auto;
-    font-size: 1rem;
-    font-weight: 400;
-    margin: 3rem 0 3rem 0;
-    padding: 0 2rem 0 2rem;
-    .Footer-content {
-      box-sizing: border-box;
-      width: 100%;
-      display: inherit;
-      height: auto;
-      .container {
-        display: inline-flex;
-        flex-wrap: wrap;
-        height: inherit;
-        margin-top: -0px;
-        margin-left: -0px;
-        width: 100%;
-        flex-direction: row;
-        padding: 0px;
-        .item-1 {
-          display: inline-flex;
-          flex-wrap: wrap;
-          width: inherit;
-          padding: 0px;
-          .item-content {
-            margin-bottom: 0.75rem;
-            a {
-              color: rgba(255, 255, 255, 0.7);
-              border-radius: 0.125rem;
-            }
-          }
-        }
-        .item-2 {
-          display: inline-flex;
-          flex-wrap: wrap;
-          width: inherit;
-          padding: 0px;
-          .item-body {
-            margin: 0.75rem 0;
-            width: 100%;
-            font-size: 0.875rem;
-            font-weight: 400;
-            .item-container {
-              display: inline-flex;
-              flex-wrap: wrap;
-              height: inherit;
-              margin-top: -1rem;
-              margin-left: -0.75rem;
-              width: calc(100% + 0.75rem);
-              flex-direction: row;
-              padding: 0px;
-              .list-item {
-                display: -webkit-inline-box;
-                display: -webkit-inline-flex;
-                display: -ms-inline-flexbox;
-                display: inline-flex;
-                -webkit-box-flex-wrap: wrap;
-                -webkit-flex-wrap: wrap;
-                -ms-flex-wrap: wrap;
-                flex-wrap: wrap;
-                width: inherit;
-                padding: 0px;
-                -webkit-flex: 0 0 calc(50% - 0.75rem);
-                -ms-flex: 0 0 calc(50% - 0.75rem);
-                flex: 0 0 calc(50% - 0.75rem);
-                margin-top: 1rem;
-                margin-left: 0.75rem;
-                margin-bottom: 14px;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+const Footer = styled.div``;
