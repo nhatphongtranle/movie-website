21563fa0f89de86a00f9dff2ec1616797381e90d
nhatphongtranle <kudo40664869@gmail.com>
2024-04-15 22:34:43 +0700

    firebase-config

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -1,16 +1,15 @@
-import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPjXQNmjZWkDgMj1egFWDrjyS5kDCFPGY",
-  authDomain: "movie-website-c10aa.firebaseapp.com",
-  projectId: "movie-website-c10aa",
-  storageBucket: "movie-website-c10aa.appspot.com",
-  messagingSenderId: "620477274088",
-  appId: "1:620477274088:web:9e1fca83b8c4f1a0b3765d",
-  measurementId: "G-KPJBLJR8LN"
+  apiKey: 'AIzaSyA8xoPtrNY67RAWjywkr2Iw9hHEAK2TtRM',
+  authDomain: 'react-netflix-clone-43739.firebaseapp.com',
+  projectId: 'react-netflix-clone-43739',
+  storageBucket: 'react-netflix-clone-43739.appspot.com',
+  messagingSenderId: '590126962951',
+  appId: '1:590126962951:web:bf789180b66ed5ad0bcda2',
+  measurementId: 'G-QRMMB0GE9J',
 };
-
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
